e5fe603550dfd01dd820b2b460a67202911925e7
Vadim <vadim.prog.2000@gmail.com>
2021-01-06 23:45:22 +0300

    create Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-const Header = () => {
-	return (
-		<header>
-			<img src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=399&h=399" alt=""/>
-		</header>
-	)
-}
+const Header = () => (
+	<header>
+		<img src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=399&h=399" alt=""/>
+	</header>
+)
 
 export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => (
+	<nav>
+		<ul>
+			<li>Profile</li>
+			<li>Messages</li>
+			<li>News</li>
+			<li>Music</li>
+		</ul>
+	</nav>
+)
+
+export default Navbar
