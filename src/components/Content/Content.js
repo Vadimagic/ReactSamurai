6df98c46934e4f92c9d31b1044e059a69c81e252
Vadim <vadim.prog.2000@gmail.com>
2021-01-07 02:12:11 +0300

    add styles for content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,7 +7,19 @@ const Content = () => (
 		</div>
 		<div className="content__info content__block">
 			<img className="content__avatar" src="https://sun9-18.userapi.com/impg/c854520/v854520979/1986cf/cI-Et1pyGsw.jpg?size=353x383&quality=96&proxy=1&sign=7d9ca54044a285792c1999c220c24750&type=album" alt="Oden"/>
-			
+			<div className="content__description"></div>
+		</div>
+		<div className="content__posts">
+			<form className="content__add-post content__block">
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+				<button>Отправить</button>
+			</form>
+			<div className="content__post content__block">
+				<div className="content__post-author">
+					<img className="content__post-avatar" src="https://sun9-28.userapi.com/impf/c840629/v840629925/732ab/rYpcddjfw1w.jpg?size=901x887&quality=96&proxy=1&sign=7695574402391908780a4b0705b103e6&type=album" alt="Avatar"/>
+				</div>
+				<div className="content__post-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequatur magnam obcaecati voluptates amet, vero facilis iure doloremque nulla perspiciatis!</div>
+			</div>
 		</div>
 	</div>
 )
