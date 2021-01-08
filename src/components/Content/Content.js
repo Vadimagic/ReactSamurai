e13f0c0833106afd621a9aea6388cab7e6174ae7
Vadim <vadim.prog.2000@gmail.com>
2021-01-09 00:05:04 +0300

    rewrite to module styles

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,26 +1,26 @@
-import './Content.css'
+import classes from './Content.module.css'
 
 const Content = () => (
-	<div className="content">
-		<div className="content__block">
-			<img className="content__img" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Background"/>
+	<div className={classes.content}>
+		<div className={classes.block}>
+			<img className={classes.img_profile} src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Background"/>
 		</div>
-		<div className="content__info content__block">
-			<img className="content__avatar" src="https://sun9-18.userapi.com/impg/c854520/v854520979/1986cf/cI-Et1pyGsw.jpg?size=353x383&quality=96&proxy=1&sign=7d9ca54044a285792c1999c220c24750&type=album" alt="Oden"/>
-			<div className="content__description"></div>
+		<div className={classes.info + ' ' + classes.block}>
+			<img className={classes.avatar} src="https://sun9-18.userapi.com/impg/c854520/v854520979/1986cf/cI-Et1pyGsw.jpg?size=353x383&quality=96&proxy=1&sign=7d9ca54044a285792c1999c220c24750&type=album" alt="Oden"/>
+			<div className={classes.description}></div>
 		</div>
-		<div className="content__posts">
-			<form className="content__add-post content__block">
-				<div className="form-block">
-				<textarea className="content__add-text"></textarea>
+		<div className={classes.posts}>
+			<form className={classes.add_post + ' ' + classes.block}>
+				<div className={classes.form_block}>
+					<textarea className={classes.add_post}></textarea>
 				</div>
-				<button className="content__submit-post">Отправить</button>
+				<button className={classes.submit_post}>Отправить</button>
 			</form>
-			<div className="content__post content__block">
-				<div className="content__post-author">
-					<img className="content__post-avatar" src="https://sun9-28.userapi.com/impf/c840629/v840629925/732ab/rYpcddjfw1w.jpg?size=901x887&quality=96&proxy=1&sign=7695574402391908780a4b0705b103e6&type=album" alt="Avatar"/>
+			<div className={classes.post + ' ' + classes.block}>
+				<div className={classes.post_author}>
+					<img className={classes.post_avatar} src="https://sun9-28.userapi.com/impf/c840629/v840629925/732ab/rYpcddjfw1w.jpg?size=901x887&quality=96&proxy=1&sign=7695574402391908780a4b0705b103e6&type=album" alt="Avatar"/>
 				</div>
-				<div className="content__post-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequatur magnam obcaecati voluptates amet, vero facilis iure doloremque nulla perspiciatis!</div>
+				<div className={classes.post_info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi consequatur magnam obcaecati voluptates amet, vero facilis iure doloremque nulla perspiciatis!</div>
 			</div>
 		</div>
 	</div>
