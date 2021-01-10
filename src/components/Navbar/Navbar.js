9a5beb5748c816d01fb741d365d01b2c60746705
Vadim <vadim.prog.2000@gmail.com>
2021-01-11 01:41:18 +0300

    add navlink for route

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,20 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Navbar.module.css'
 
 const Navbar = () => (
 	<nav className={classes.navbar}>
 		<ul className={classes.list}>
 			<li className={classes.item}>
-				<a className={classes.link} href="/profile">Профиль</a>
+				<NavLink className={classes.link} to="/profile">Профиль</NavLink>
 			</li>
 			<li className={classes.item}>
-				<a className={classes.link} href="/messenger">Сообщения</a>
+				<NavLink className={classes.link} to="/messenger">Сообщения</NavLink>
 			</li>
 			<li className={classes.item}>
-				<a className={classes.link} href="/news">Новости</a>
+				<NavLink className={classes.link} to="/news">Новости</NavLink>
 			</li>
 			<li className={classes.item}>
-				<a className={classes.link} href="/music">Музыка</a>
+				<NavLink className={classes.link} to="/music">Музыка</NavLink>
 			</li>
 		</ul>
 	</nav>
