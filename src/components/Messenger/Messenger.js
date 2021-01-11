c597e1bb8a78cd8fd820f9f70520dc874361e7db
Vadim <vadim.prog.2000@gmail.com>
2021-01-11 23:57:47 +0300

    create structure Messenger

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -3,7 +3,22 @@ import classes from './Messenger.module.css'
 const Messenger = () => {
 	return (
 		<div className={classes.content}>
-			Dialogs
+			<div className={classes.list_dialogs}>
+				<div className={classes.item}>
+					Vadim
+				</div>
+				<div className={classes.item}>
+					Dima
+				</div>
+				<div className={classes.item}>
+					Dima
+				</div>
+			</div>
+			<div clasaName={classes.messages}>
+				<div className={classes.message}>Привет</div>
+				<div className={classes.message}>Как дела?</div>
+				<div className={classes.message}>Что делаешь?</div>
+			</div>
 		</div>
 	)
 }
