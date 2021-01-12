95fb1561eee947d632207fc2d968adc27de0e9dd
Vadim <vadim.prog.2000@gmail.com>
2021-01-12 22:38:25 +0300

    complete dialogs

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -7,19 +7,25 @@ const Messenger = () => {
 				<div className={classes.dialog}>
 					<img className={classes.dialog__photo} src="https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
-						Vadim
+						<div className={classes.dialog__name}>
+							Vadim
+						</div>
 					</div>
 				</div>
-				<div className={classes.dialog}>
+				<div className={classes.dialog + ' ' + classes.dialog_active}>
 					<img className={classes.dialog__photo} src="https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
-						Dima
+						<div className={classes.dialog__name}>
+							Dima
+						</div>
 					</div>
 				</div>
 				<div className={classes.dialog}>
 					<img className={classes.dialog__photo} src="https://img06.rl0.ru/afisha/e350x350p492x0f1080x1080q85i/s2.afisha.ru/mediastorage/3d/a7/c12ce10ae3e14a23a62b792aa73d.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
-						Semen
+						<div className={classes.dialog__name}>
+							Semen
+						</div>
 					</div>
 				</div>
 			</div>
