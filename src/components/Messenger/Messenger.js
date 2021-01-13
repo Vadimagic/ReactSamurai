dc486202d2b8cb402e2d1595ca57d47a89153bfa
Vadim <vadim.prog.2000@gmail.com>
2021-01-13 23:29:33 +0300

    create navlink dialogs

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -1,33 +1,34 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Messenger.module.css'
 
 const Messenger = () => {
 	return (
 		<div className={classes.block}>
 			<div className={classes.dialogs}>
-				<div className={classes.dialog}>
+				<NavLink to="/messenger/34fetqsg" className={classes.dialog}>
 					<img className={classes.dialog__photo} src="https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
 						<div className={classes.dialog__name}>
-							Вадим
+							Вадим 
 						</div>
 					</div>
-				</div>
-				<div className={classes.dialog + ' ' + classes.dialog_active}>
+				</NavLink>
+				<NavLink to="/messenger/6f4tfgvw" className={classes.dialog + ' ' + classes.dialog_active}>
 					<img className={classes.dialog__photo} src="https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
 						<div className={classes.dialog__name}>
 							Дима
 						</div>
 					</div>
-				</div>
-				<div className={classes.dialog}>
+				</NavLink>
+				<NavLink to="/messenger/z773pds3" className={classes.dialog}>
 					<img className={classes.dialog__photo} src="https://img06.rl0.ru/afisha/e350x350p492x0f1080x1080q85i/s2.afisha.ru/mediastorage/3d/a7/c12ce10ae3e14a23a62b792aa73d.jpg" alt="Avatar"/>
 					<div className={classes.dialog__content}>
 						<div className={classes.dialog__name}>
 							Семён
 						</div>
 					</div>
-				</div>
+				</NavLink>
 			</div>
 			<div className={classes.messages}>
 				<div className={classes.message}>
