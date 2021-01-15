9dd2b95d6626071d8c7aedee5e723072bae80d4c
Vadim <vadim.prog.2000@gmail.com>
2021-01-15 23:58:51 +0300

    complete style for profile info

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,18 +7,18 @@ const Content = () => (
 			<img className={classes.img_profile} src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Background"/>
 		</div>
 		<div className={classes.info + ' ' + classes.block}>
-			<img className={classes.avatar} src="https://sun9-18.userapi.com/impg/c854520/v854520979/1986cf/cI-Et1pyGsw.jpg?size=353x383&quality=96&proxy=1&sign=7d9ca54044a285792c1999c220c24750&type=album" alt="Oden"/>
+			<img className={classes.avatar} src="https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg" alt="avatar"/>
 			<div className={classes.description}>
 				<h1 className={classes.name}>Вадим</h1>
 				<span className={classes.thought}>“Shoot for the moon. Even if you miss, you'll land among the stars.”</span>
 				<ul className={classes.social}>
 					<li className={classes.item + ' ' + classes.friends}>
-						<span className={classes.friends_count}>46</span>
-						<span className={classes.friends_text}>друзья</span>
+						<span className={classes.count}>46</span>
+						<span className={classes.text}>друзья</span>
 					</li>
 					<li className={classes.item + ' ' + classes.posts}>
-						<span className={classes.posts_count}>2</span>
-						<span className={classes.posts_text}>посты</span>
+						<span className={classes.count}>2</span>
+						<span className={classes.text}>посты</span>
 					</li>
 				</ul>
 			</div>
