2df43de9053793f3fecb0631ddac595dfc77326a
Vadim <vadim.prog.2000@gmail.com>
2021-01-19 23:53:41 +0300

    logo to link(profile)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Header.module.css'
 
 const Header = () => (
 	<header className={classes.header}>
 		<div className={classes.content}>
-			<img className={classes.image} src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=399&h=399" alt=""/>
+			<NavLink to="/profile">
+				<img className={classes.image} src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=399&h=399" alt=""/>
+			</NavLink>
 		</div>
 	</header>
 )
