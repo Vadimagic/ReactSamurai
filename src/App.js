67667582591570a67213b4378ebcb22f23cf721a
Vadim <vadim.prog.2000@gmail.com>
2021-01-20 23:23:41 +0300

    transfer data (fialogs + example messages) to App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ function App() {
 		content: {text: '', image: 'https://lh3.googleusercontent.com/7PtRH83w1K13OpR4JLotEhogmIvWHTggpsgHThN-LPzLfOPR9RnpQXLT86KEhS-kQ56NifE80vo6FekpScb4Co5HhFM=w640-h400-e365-rj-sc0x00ffffff'}, likesCount: 3}
 	]
 
+	const messangerDialogs = [
+		{id: '34fetqsg', name: 'Вадим', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg'},
+		{id: '6f4tfgvw', name: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg'},
+		{id: 'z773pds3', name: 'Семен', avatar: 'https://img06.rl0.ru/afisha/e350x350p492x0f1080x1080q85i/s2.afisha.ru/mediastorage/3d/a7/c12ce10ae3e14a23a62b792aa73d.jpg'},
+	]
+
+	const exampleMessagesForDialog = [
+		{id: '142ds2wf', author: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg', text: 'Привет'},
+		{id: '234rwesd', author: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg', text: 'Как дела?'},
+		{id: 'sdvd2r32', author: 'Вадим', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', text: 'Хорошо, <br/>А у тебя?'},
+	]
+
 	return (
 		<BrowserRouter>
 			<Header />
@@ -22,7 +34,7 @@ function App() {
 				<div className="wrapper__content">
 					<Switch>
 						<Route path="/profile" render={() => <Profile posts={profilePosts}/>}/>
-						<Route path="/messenger" component={Messenger}/>
+						<Route path="/messenger" render={() => <Messenger dialogs={messangerDialogs} messages={exampleMessagesForDialog}/>}/>
 						<Redirect to="messenger"/>
 					</Switch>
 				</div>
--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -2,19 +2,11 @@ import DialogsList from './DialogsList/DialogsList'
 import MessagesList from './MessagesList/MessagesList'
 import classes from './Messenger.module.css'
 
-const Messenger = () => {
+const Messenger = ({dialogs, messages}) => {
 	return (
 		<div className={classes.block}>
-			<DialogsList dialogs={[
-				{id: '34fetqsg', name: 'Вадим', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg'},
-				{id: '6f4tfgvw', name: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg'},
-				{id: 'z773pds3', name: 'Семен', avatar: 'https://img06.rl0.ru/afisha/e350x350p492x0f1080x1080q85i/s2.afisha.ru/mediastorage/3d/a7/c12ce10ae3e14a23a62b792aa73d.jpg'},
-			]}/>
-			<MessagesList messages={[
-				{id: '142ds2wf', author: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg', text: 'Привет'},
-				{id: '234rwesd', author: 'Дима', avatar: 'https://avatanplus.com/files/photos/mid/59a2b4633c3f415e2390a3af.jpg', text: 'Как дела?'},
-				{id: 'sdvd2r32', author: 'Вадим', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', text: 'Хорошо, <br/>А у тебя?'},
-			]}/>
+			<DialogsList dialogs={dialogs}/>
+			<MessagesList messages={messages}/>
 		</div>
 	)
 }
