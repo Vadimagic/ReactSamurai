e2734f0f861d109facd6f3470fc46fdd2a0c3351
Vadim <vadim.prog.2000@gmail.com>
2021-01-20 23:19:35 +0300

    transfer data (profile posts) to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import './App.css'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
+	const profilePosts = [
+		{id: 'GS3sfe342S', author: 'Вадим', authorId: '21wddscfa', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', 
+		content: {text: 'Как прекрасно жить в этом мире, вы согласны?', image: 'https://wallscloud.net/uploads/cache/914086997/anime-art-devushka-spina-1024x576-MM-90.jpg'}, likesCount: 3},
+		{id: 'GD32dSFgdd', author: 'Вадим', authorId: '21wddscfa', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', 
+		content: {text: '', image: 'https://lh3.googleusercontent.com/7PtRH83w1K13OpR4JLotEhogmIvWHTggpsgHThN-LPzLfOPR9RnpQXLT86KEhS-kQ56NifE80vo6FekpScb4Co5HhFM=w640-h400-e365-rj-sc0x00ffffff'}, likesCount: 3}
+	]
+
 	return (
 		<BrowserRouter>
 			<Header />
@@ -14,7 +21,7 @@ function App() {
 				<Navbar />
 				<div className="wrapper__content">
 					<Switch>
-						<Route path="/profile" component={Profile}/>
+						<Route path="/profile" render={() => <Profile posts={profilePosts}/>}/>
 						<Route path="/messenger" component={Messenger}/>
 						<Redirect to="messenger"/>
 					</Switch>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import classes from './Profile.module.css'
 import PostsList from './PostsList/PostsList'
 
-const Content = () => (
+const Content = ({posts}) => (
 	<>
 		<div className={classes.block}>
 			<img className={classes.img_profile} src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="Background"/>
@@ -23,12 +23,7 @@ const Content = () => (
 				</ul>
 			</div>
 		</div>
-		<PostsList posts={[
-			{id: 'GS3sfe342S', author: 'Вадим', authorId: '21wddscfa', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', 
-			content: {text: 'Как прекрасно жить в этом мире, вы согласны?', image: 'https://wallscloud.net/uploads/cache/914086997/anime-art-devushka-spina-1024x576-MM-90.jpg'}, likesCount: 3},
-			{id: 'GD32dSFgdd', author: 'Вадим', authorId: '21wddscfa', avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', 
-			content: {text: '', image: 'https://lh3.googleusercontent.com/7PtRH83w1K13OpR4JLotEhogmIvWHTggpsgHThN-LPzLfOPR9RnpQXLT86KEhS-kQ56NifE80vo6FekpScb4Co5HhFM=w640-h400-e365-rj-sc0x00ffffff'}, likesCount: 3}
-		]}/>
+		<PostsList posts={posts}/>
 	</>
 )
 
