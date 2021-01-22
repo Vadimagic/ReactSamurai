104e38b243f7048eaf4d3f494e894685be1d64c8
Vadim <vadim.prog.2000@gmail.com>
2021-01-22 23:59:14 +0300

    add div text for add posts

--- src/components/Messenger/MessagesList/AddMessage/AddMessage.js
+++ src/components/Messenger/MessagesList/AddMessage/AddMessage.js
@@ -2,7 +2,14 @@ import classes from './AddMessage.module.css'
 
 const AddMessage = () => {
 	return (
-		<div className={classes.textarea} tabIndex="0" role="textbox" aria-multiline="true" contentEditable="PLAINTEXT-ONLY" data-role="editable" aria-label="Присоединиться к обсуждению...">
+		<div 
+			className={classes.textarea} 
+			tabIndex="0" role="textbox" 
+			aria-multiline="true" 
+			contentEditable="PLAINTEXT-ONLY" 
+			data-role="editable" 
+			aria-label="Присоединиться к обсуждению..."
+		>
 		</div>
 	)
 }
--- src/components/Profile/PostsList/AddPost/AddPost.js
+++ src/components/Profile/PostsList/AddPost/AddPost.js
@@ -1,14 +1,9 @@
-import { useState } from 'react'
 import classes from './AddPost.module.css'
 
 const AddPost = () => {
-	const [text, setText] = useState('')
-
 	return (
 		<form className={classes.add_post + ' ' + classes.block}>
-			<div className={classes.form_block}>
-				<textarea value={text} className={classes.add_text} onChange={(e) => setText(e.target.value)}></textarea>
-			</div>
+			<div className={classes.textarea} tabIndex="0" role="textbox" aria-multiline="true" contentEditable="PLAINTEXT-ONLY" data-role="editable" aria-label="Присоединиться к обсуждению..."></div>
 			<button className={classes.submit_post}>Отправить</button>
 		</form>
 	)
