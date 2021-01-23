805522f538d3d8b01cdc075f6d4f47821ae742a1
Vadim <vadim.prog.2000@gmail.com>
2021-01-23 23:23:40 +0300

    create addPost

--- src/redux/state.js
+++ src/redux/state.js
@@ -23,4 +23,16 @@ const state = {
 	exampleMessagesForDialog
 }
 
+export const addPost = (postText = '', postImage = '') =>  {
+	const newPost = {
+		id: String(Date.now()), 
+		author: 'Вадим', 
+		authorId: 'vadimagic', 
+		avatar: 'https://images-na.ssl-images-amazon.com/images/I/712dV%2BdZpdL._AC_SY679_.jpg', 
+		content: {text: postText, image: postImage}, 
+		likesCount: 0
+	}
+	state.profilePosts.push(newPost)
+}
+
 export default state
