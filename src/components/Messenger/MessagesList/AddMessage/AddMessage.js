d601cb5b128ac8794b5272cd7ced5742bfc7ac81
Vadim <vadim.prog.2000@gmail.com>
2021-01-24 23:56:18 +0300

    create state for contentEditable(Message)

--- src/components/Messenger/MessagesList/AddMessage/AddMessage.js
+++ src/components/Messenger/MessagesList/AddMessage/AddMessage.js
@@ -1,16 +1,22 @@
+import { useState } from 'react'
 import classes from './AddMessage.module.css'
 
 const AddMessage = () => {
+	const [textMessage, setTextMessage] = useState('')
+
+	const updateMessageText = (e) => {
+		setTextMessage(e.target.innerText)
+		console.log(textMessage)
+	}
+
 	return (
 		<div 
 			className={classes.textarea} 
-			tabIndex="0" role="textbox" 
 			aria-multiline="true" 
-			contentEditable="PLAINTEXT-ONLY" 
-			data-role="editable" 
-			aria-label="Присоединиться к обсуждению..."
-		>
-		</div>
+			contentEditable={true}
+			onInput={updateMessageText}
+			onBlur={updateMessageText}
+		></div>
 	)
 }
 
