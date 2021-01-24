d2e72dccab4a5fdf74a5dbc0fe5153ec137b3a88
Vadim <vadim.prog.2000@gmail.com>
2021-01-24 23:50:44 +0300

    create state for contentEditable div

--- src/components/Profile/PostsList/AddPost/AddPost.js
+++ src/components/Profile/PostsList/AddPost/AddPost.js
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import {addPost} from '../../../../redux/state'
 import classes from './AddPost.module.css'
 
 const AddPost = () => {
+	const [text, setText] = useState('')
 	
 	const submitPost = (e) => {
 		e.preventDefault()
-		addPost(e.target.children[0].innerText)
-		e.target.children[0].innerText = ''
+		addPost(text)
+		setText('')
+	}
+
+	const updateText = (e) => {
+		setText(e.target.innerText)
 	}
 
 	return (
@@ -15,7 +21,8 @@ const AddPost = () => {
 				className={classes.textarea} 
 				aria-multiline="true" 
 				contentEditable={true}
-				suppressContentEditableWarning={true}
+				onInput={updateText}
+				onBlur={updateText}
 			></div>
 			<button className={classes.submit_post}>Отправить</button>
 		</form>
