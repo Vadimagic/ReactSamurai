b90e3109a88b3c47656161ea9c49d8b5526e959f
Vadim <vadim.prog.2000@gmail.com>
2021-01-26 23:38:28 +0300

    add title document for pages

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -1,8 +1,13 @@
+import { useEffect } from 'react'
 import DialogsList from './DialogsList/DialogsList'
 import MessagesList from './MessagesList/MessagesList'
 import classes from './Messenger.module.css'
 
 const Messenger = ({messengerState}) => {
+	useEffect(() => {
+		document.title = 'Сообщения'
+	}, [])
+
 	return (
 		<div className={classes.block}>
 			<DialogsList dialogs={messengerState.messengerDialogs}/>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,17 @@
+import { useEffect } from 'react'
 import PostsList from './PostsList/PostsList'
 import { ProfileInfo } from './ProfileInfo/ProfileInfo'
 
-const Content = ({profileState}) => (
-	<>
-		<ProfileInfo />
-		<PostsList posts={profileState.profilePosts}/>
-	</>
-)
+const Content = ({profileState}) => {
+	useEffect(() => {
+		document.title = 'Профиль'
+	}, [])
+	return (
+		<>
+			<ProfileInfo />
+			<PostsList posts={profileState.profilePosts}/>
+		</>
+	)
+}
 
 export default Content
